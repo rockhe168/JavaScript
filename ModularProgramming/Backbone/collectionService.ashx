feb9d4e1aaf7d9867941ac554cc1dca39348fc69
rockhe168 <rockhe@gmail.com>
2014-04-26 16:48:38 +0800

    backbone helloword，model，collection

---
+++ ModularProgramming/Backbone/collectionService.ashx
@@ -0,0 +1,1 @@
+﻿<%@ WebHandler Language="C#" CodeBehind="collectionService.ashx.cs" Class="Backbone.collectionService" %>
---
+++ ModularProgramming/Backbone/collectionService.ashx.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+
+namespace Backbone
+{
+    /// <summary>
+    /// collectionService 的摘要说明
+    /// </summary>
+    public class collectionService : IHttpHandler
+    {
+
+        public void ProcessRequest(HttpContext context)
+        {
+            context.Response.ContentType = "application/json";
+             // Post方式下，取得client端传过来的数据  
+            if ("post".Equals(context.Request.HttpMethod.ToLower()))
+            {
+                StreamReader reader = new StreamReader(context.Request.InputStream);
+                string json = HttpUtility.UrlDecode(reader.ReadToEnd());
+                context.Response.Write(json);
+            }
+            // Get方式下，取得client端传过来的数据  
+            else
+            {
+                context.Response.Write("[{\"title\":\"Java高级编程\"},{\"title\":\"C#高级编程\"},{\"title\":\"JavaScript高级编程\"}]");
+            }
+        }
+
+        public bool IsReusable
+        {
+            get
+            {
+                return false;
+            }
+        }
+    }
+}
