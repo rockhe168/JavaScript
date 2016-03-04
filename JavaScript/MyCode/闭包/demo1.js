5231f26f1cce9f36f433ca72c58932916cf38885
何湘红 <xhhe@Ctrip.com>
2016-03-04 17:00:28 +0800

    update

---
+++ JavaScript/MyCode/闭包/demo1.js
@@ -0,0 +1,27 @@
+﻿
+
+function f1(){
+
+	var b = 99;
+
+    add = function (){
+		b = b +1;
+	};
+
+  
+    function f2 (){
+       console.info(b);
+    }
+
+    return f2;
+}
+
+var result = f1();
+
+//add();
+
+result();
+
+add();
+
+result();
